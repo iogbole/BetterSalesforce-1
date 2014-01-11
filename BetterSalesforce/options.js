77acd6ac64def17c6fc8e0d610cfcec95c360ce0
Josh Leckbee <josh.leckbee@gmail.com>
2014-01-10 17:40:55 -0800

    Add option functionality

--- BetterSalesforce/options.js
+++ BetterSalesforce/options.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+
+    // Refresh rate
+    $("#range_input").change(function() {
+        var newval = $(this).val();
+        localStorage.refreshTime = newval;
+        $("#slidernumber").text(newval);
+    });
+
+    // Alert on P1 CB
+    $("#check_alertp1").change(function() {
+       localStorage.alertP1 = this.checked;
+    })
+
+    // Minimilist View CB
+    $("#check_minimilist").change(function() {
+        localStorage.minimilist = this.checked;
+    })
 });
 
 function save_options() {
@@ -16,14 +33,17 @@ function flashStatus(msg) {
 }
 
 function restore_options() {
-    return;
-}
+    // Refresh rate
+    var refreshTime = localStorage.refreshTime ? localStorage.refreshTime : 60;
+    $("#slidernumber").text(refreshTime);
+    $("#range_input").attr("value", refreshTime);
 
-function changeRefresh() {
-    $("label#range_output").innerHTML = localStorage.refreshTime;
+    // Show alert on P1
+    $("#check_alertp1").prop('checked', localStorage.alertP1 == 'true');
+
+    // Minimilst
+    $("#check_minimilist").prop('checked', localStorage.minimilist == 'true');
+
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
-$("#range_input").on('change', function() {
-    changeRefresh.call($(this));
-});
