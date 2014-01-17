4f53e0e8a38e1ffd01660fb2686671ba53cdbc00
Josh Leckbee <josh.leckbee@gmail.com>
2014-01-16 16:45:51 -0800

    Refactor localStorage.mode, add JC comment navigation

--- BetterSalesforce/bettercommunity_script.js
+++ BetterSalesforce/bettercommunity_script.js
@@ -16,37 +16,47 @@ function checkForComments() {
 function addLastCommentLink() {
     $('span.j-thread-info-block.font-color-meta a').clone().appendTo($('div.j-byline.font-color-meta').append(' - '));
     $('div.j-byline.font-color-meta a').on('click', function() {
-        setTimeout(function(){
-            addCommentNavigation();
-            addPaginationClickEvent();
-        },1500);
+        checkForLoadingComments();
     });
 }
 
 function addCommentNavigation() {
-    if ($('.comment-nav').length) { console.log("FOUND COM"); return; }
-    $('p.jive-comment-meta.font-color-meta-light').append("<div class='comment-nav' style='float:right;display:none;'><a class='prev-comment' style='display:inline-box;cursor:pointer;margin-right:5px;'>Previous</a><a class='next-comment' style='display:inline-box;cursor:pointer;'>Next</a></div>");
-    $('a.next-comment').on('click', function() {
-        var pos = $(this).closest('li.reply').nextAll('li.reply').offset().top;
-        $('html, body').scrollTop(pos);
-    });
-    $('a.prev-comment').on('click', function() {
-        var pos = $(this).closest('li.reply').prevAll('li.reply').offset().top;
-        $('html, body').scrollTop(pos);
-    });
-    $('.reply').on('mouseover', function() {
-        $(this).find('.comment-nav').show();
+    $('.reply').on('mouseenter', function() {
+        $(this).find('.jive-comment-content').prepend("<span class='comment-nav'><label class='prev-comment'>Previous</label><label class='next-comment'>Next</label></span>");
+        $('.prev-comment').on('click', function() {
+            var pos;
+            try {
+                pos = $(this).closest('li.reply').prevAll('li.reply').offset().top;
+            } catch (err) {
+                pos = $('.j-comment-action-bar').offset().top;
+            }
+            $('html, body').scrollTop(pos);
+        });
+        $('.next-comment').on('click', function() {
+            var pos = $(this).closest('li.reply').nextAll('li.reply').offset().top;
+            $('html, body').scrollTop(pos);
+        });
     });
 
-    $('.reply').on('mouseout', function() {
-        $(this).find('.comment-nav').hide();
+    $('.reply').on('mouseleave', function() {
+        $(this).find('.comment-nav').remove();
     });
 }
 
 function addPaginationClickEvent() {
     $('span.j-pagination').on('click', 'a', function() {
-        setTimeout(function(){addCommentNavigation(); addPaginationClickEvent()}, 1500);
+        checkForLoadingComments();
     });
+}
+
+function checkForLoadingComments() {
+    var myInt = setInterval(function(){
+        if (!$('.j-running-loader').length) {
+            addCommentNavigation();
+            addPaginationClickEvent()
+            clearInterval(myInt);
+        }
+    }, 750);
 }
 
 var waitForCommentsTimer = setInterval(function(){checkForComments();}, 250);
